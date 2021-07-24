61489c70552e19bea0d6065c2877d74db43cbb79
Yogesh Rathi <=>
2021-07-24 18:34:56 +0530

    Product component added

---
+++ src/app/shared/components/banner-card/banner-card.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-banner-card',
+  templateUrl: './banner-card.component.html',
+  styleUrls: ['./banner-card.component.scss']
+})
+export class BannerCardComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
